72dcd8ddc195ca7ab96c1439026b1b8be483f547
kondo0602 <kondo8363@gmail.com>
2023-11-04 22:03:06 +0900

    feat: add outline button style

--- examples/next-app/src/app/page.tsx
+++ examples/next-app/src/app/page.tsx
@@ -6,19 +6,25 @@ const Home = () => {
       <h1>Welcome to Nostalgic-UI.</h1>
       <div>
         <button className={button({ color: 'blue' })}>Blue</button>
-        <button className={button({ color: 'red' })}>Red</button>
         <button className={button({ color: 'green' })}>Green</button>
+        <button className={button({ color: 'red' })}>Red</button>
         <button className={button({ color: 'yellow' })}>Yellow</button>
         <button className={button({ color: 'white' })}>White</button>
-        <button className={button({ color: 'blue' })} disabled>Disabled</button>
+        <button className={button()} disabled>Disabled</button>
       </div>
       <div>
         <button className={button({ color: 'blue', size: 'sm' })}>Blue</button>
-        <button className={button({ color: 'red', size: 'sm' })}>Red</button>
         <button className={button({ color: 'green', size: 'sm' })}>Green</button>
+        <button className={button({ color: 'red', size: 'sm' })}>Red</button>
         <button className={button({ color: 'yellow', size: 'sm' })}>Yellow</button>
         <button className={button({ color: 'white', size: 'sm' })}>White</button>
-        <button className={button({ color: 'blue', size: 'sm' })} disabled>Disabled</button>
+        <button className={button({ size: 'sm' })} disabled>Disabled</button>
+      </div>
+      <div>
+        <button className={button({ color: 'blue', variant: 'outline' })}>Blue</button>
+        <button className={button({ color: 'green', variant: 'outline' })}>Green</button>
+        <button className={button({ color: 'red', variant: 'outline' })}>Red</button>
+        <button className={button({ color: 'yellow', variant: 'outline' })}>Yellow</button>
       </div>
     </main>
   );
--- packages/css/src/styles/button.css.ts
+++ packages/css/src/styles/button.css.ts
@@ -25,38 +25,10 @@ export const button = recipe({
   ],
   variants: {
     color: {
-      blue: {
-        backgroundColor: theme.color.sky500,
-        color: theme.color.white,
-        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.blue700}`,
-        ':active': {
-          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.blue700}`,
-        },
-      },
-      red: {
-        backgroundColor: theme.color.red500,
-        color: theme.color.white,
-        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.red700}`,
-        ':active': {
-          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.red700}`,
-        },
-      },
-      yellow: {
-        backgroundColor: theme.color.yellow400,
-        color: theme.color.black,
-        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.yellow600}`,
-        ':active': {
-          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.yellow600}`,
-        },
-      },
-      green: {
-        backgroundColor: theme.color.lime500,
-        color: theme.color.white,
-        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.green700}`,
-        ':active': {
-          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.green700}`,
-        },
-      },
+      blue: {},
+      green: {},
+      red: {},
+      yellow: {},
       white: {
         backgroundColor: theme.color.white,
         color: theme.color.black,
@@ -66,13 +38,131 @@ export const button = recipe({
         },
       },
     },
+    variant: {
+      solid: {},
+      outline: {},
+      ghost: {},
+    },
     size: {
       sm: { padding: '6px 10px' },
       md: { padding: '10px 12px' },
     },
   },
+
+  compoundVariants: [
+    {
+      variants: {
+        color: 'blue', variant: 'solid',
+      },
+      style: {
+        backgroundColor: theme.color.sky500,
+        color: theme.color.white,
+        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.blue700}`,
+        ':active': {
+          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.blue700}`,
+        },
+      },
+    },
+    {
+      variants: {
+        color: 'blue', variant: 'outline',
+      },
+      style: {
+        backgroundColor: 'transparent',
+        color: theme.color.sky500,
+        borderColor: theme.color.sky500,
+        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.slate300}`,
+        ':active': {
+          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.slate300}`,
+        },
+      },
+    },
+    {
+      variants: {
+        color: 'green', variant: 'solid',
+      },
+      style: {
+        backgroundColor: theme.color.lime500,
+        color: theme.color.white,
+        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.green700}`,
+        ':active': {
+          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.green700}`,
+        },
+      },
+    },
+    {
+      variants: {
+        color: 'green', variant: 'outline',
+      },
+      style: {
+        backgroundColor: 'transparent',
+        color: theme.color.lime500,
+        borderColor: theme.color.lime500,
+        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.slate300}`,
+        ':active': {
+          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.slate300}`,
+        },
+      },
+    },
+    {
+      variants: {
+        color: 'red', variant: 'solid',
+      },
+      style: {
+        backgroundColor: theme.color.red500,
+        color: theme.color.white,
+        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.red700}`,
+        ':active': {
+          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.red700}`,
+        },
+      },
+    },
+    {
+      variants: {
+        color: 'red', variant: 'outline',
+      },
+      style: {
+        backgroundColor: 'transparent',
+        color: theme.color.red500,
+        borderColor: theme.color.red500,
+        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.slate300}`,
+        ':active': {
+          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.slate300}`,
+        },
+      },
+    },
+    {
+      variants: {
+        color: 'yellow', variant: 'solid',
+      },
+      style: {
+        backgroundColor: theme.color.yellow400,
+        color: theme.color.black,
+        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.yellow600}`,
+        ':active': {
+          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.yellow600}`,
+        },
+      },
+    },
+    {
+      variants: {
+        color: 'yellow', variant: 'outline',
+      },
+      style: {
+        backgroundColor: 'transparent',
+        color: theme.color.yellow400,
+        borderColor: theme.color.yellow400,
+        boxShadow: `inset ${theme.border.mdNegative} ${theme.border.mdNegative} ${theme.color.slate300}`,
+        ':active': {
+          boxShadow: `inset ${theme.border.md} ${theme.border.md} ${theme.color.slate300}`,
+        },
+      },
+    },
+  ],
+
   defaultVariants: {
     color: 'blue',
+    variant: 'solid',
     size: 'md',
   },
 });
