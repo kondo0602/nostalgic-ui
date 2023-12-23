2d93b3351dd05e8d556e8e0d790e3b4dae47aa79
kondo0602 <kondo8363@gmail.com>
2023-12-24 01:03:26 +0900

    Add border-square utility class to tailwind.config.js

--- packages/tailwind/tailwind.config.js
+++ packages/tailwind/tailwind.config.js
@@ -1,8 +1,26 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      addUtilities({
+        ".border-square": {
+          "border-width": "4px",
+          "border-style": "solid",
+          "border-color": "black",
+          "clipPath": `polygon(
+            0 4px, 4px 4px, 4px 0,
+            calc(100% - 4px) 0, calc(100% - 4px) 4px, 100% 4px,
+            100% calc(100% - 4px), calc(100% - 4px) calc(100% - 4px), calc(100% - 4px) 100%,
+            4px 100%, 4px calc(100% - 4px), 0 calc(100% - 4px)
+          )`,
+        }
+      })
+    }),
+  ],
 }
